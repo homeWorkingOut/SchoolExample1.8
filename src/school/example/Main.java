132bee75253a875db49a92ffb828479e6c7ef4c4
Lisitsa_n@mail.ru <Lisitsa_n@mail.ru>
2018-01-09 17:52:49 +0400

    чтение и запись java 8

--- src/school/example/Main.java
+++ src/school/example/Main.java
@@ -11,12 +11,14 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String myJarPath = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        String dirPath = new File(myJarPath).getParent();
-        System.out.println(dirPath);
-        Path pt = Paths.get (dirPath, "test.txt");
-        System.out.println(pt);
-        SetterTextFile.PutDataFile(pt);
-        GetterTextFile.GetDataFile(pt);
+//        String myJarPath = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+//        String dirPath = new File(myJarPath).getParent();
+//        System.out.println(dirPath);
+//        Path pt = Paths.get (dirPath, "test.txt");
+//        System.out.println(pt);
+//        SetterTextFile.PutDataFile(pt);
+//        GetterTextFile.GetDataFile(pt);
+   System.out.println("Введите номер оперции: \n1 ввод данных, \n2 вывод данных по школе, \n3 правка оценок в журнале");
+
     }
 }
