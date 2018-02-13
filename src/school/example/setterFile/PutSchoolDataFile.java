88d8208cb8f364506437473fbac53f809ef3209f
Lisitsa_n@mail.ru <Lisitsa_n@mail.ru>
2018-02-13 16:40:48 +0400

    ввод данных java 8 write file

--- src/school/example/setterFile/PutSchoolDataFile.java
+++ src/school/example/setterFile/PutSchoolDataFile.java
@@ -3,26 +3,33 @@ package school.example.setterFile;
 import school.example.persons.Person;
 import school.example.school.SchoolClass;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class PutSchoolDataFile {
     public static void PutDataFiles (List<SchoolClass> school) {
-        for (int i = 0; i< school.size(); i++){
+        List<String> spData = new ArrayList<>();
+        for (int i = 0; i< school.size(); i++) {
             SchoolClass classS = school.get(i);
             System.out.println(classS.getId().getClassNumber());
-            System.out.println(classS.getId().getClassLetter());
+            spData.add(String.valueOf(classS.getId().getClassNumber())  + "\n");
+            System.out.println(classS.getId().getClassLetter()  + "\n");
+            spData.add(classS.getId().getClassLetter());
             System.out.println(classS.getTeacherClass().getName());
             System.out.println(classS.getTeacherClass().getPatronymic());
             System.out.println(classS.getTeacherClass().getSurname());
-            List<Person> ListSchoolBoy = classS.getListSchoolBoy();
+            spData.add(classS.getTeacherClass().getName() + "\t" + classS.getTeacherClass().getPatronymic() + "\t" + classS.getTeacherClass().getSurname() + "\n");
 
+            List<Person> ListSchoolBoy = classS.getListSchoolBoy();
             for (Iterator<Person> iter = ListSchoolBoy.iterator(); iter.hasNext(); ) {
                 Person child = iter.next();
                 System.out.println("Ученик " + child.getName() + " " + child.getSurname());
+                spData.add(child.getName() + "\t" + child.getPatronymic() + "\t" + child.getSurname() + "\n");
             }
-        }
 
+        }
+        SetterTextFile.PutDataFile(spData);
     }
 
 }
--- src/school/example/setterFile/SetterTextFile.java
+++ src/school/example/setterFile/SetterTextFile.java
@@ -1,13 +1,20 @@
 package school.example.setterFile;
 
+import school.example.publicVariable.PublicVariable;
+
 import java.io.*;
 import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.List;
 
 public class SetterTextFile {
-    public static void PutDataFile(Path fileName) {
+    public static void PutDataFile(List <String> spData) {
+        Path fileName = PublicVariable.PATHFILEDATA;
+        System.out.println(fileName);
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(String.valueOf(fileName)))) {
-            String text = "Привет мир!";
-            bw.write(text);
+         for (Iterator <String> iter = spData.iterator(); iter.hasNext();) {
+             bw.write(iter.next());
+         }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
